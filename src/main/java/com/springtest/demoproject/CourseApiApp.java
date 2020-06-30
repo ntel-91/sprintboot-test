15043eb7aee38eebe19305053e390d8ae029b735
ntel-91 <ntelenson@gmail.com>
2020-06-30 10:33:16 -0400

    first commit

---
+++ src/main/java/com/springtest/demoproject/CourseApiApp.java
@@ -0,0 +1,11 @@
+package com.springtest.demoproject;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+    public static void main(String[] args){
+        SpringApplication.run(CourseApiApp.class, args);
+    }
+}
